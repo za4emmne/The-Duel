e57a9f6636f14964bb3506a9383ef9dd6b03815f
za4emmne <iluhaha@mail.ru>
2026-03-24 00:46:34 +0500

    ver 0.01
    add player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,31 +4,62 @@ using UnityEngine;
 [RequireComponent((typeof(CharacterController)))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _speed;
+    [SerializeField] private float _speed = 8f;
+    [SerializeField] private float _straveSpeed = 7f;
+    [SerializeField] private float _jumpSpeed = 9f;
+    [SerializeField] private float _gravityFactor = 2f;
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private float __horizontalSensivity = 7f;
+    [SerializeField] private float _verticalSensivity = 10f;
+    [SerializeField] private float _verticalMinAngle = -89f;
+    [SerializeField] private float _verticalMaxAngle = 89f;
     
+    private Vector3 _verticalVelocity;
     private Transform _transform;
-    private CharacterController _characterController;   
+    private CharacterController _characterController;
+    private float _cameraAngle = 0;
 
     private void Awake()
-    {
+    { 
         _characterController = GetComponent<CharacterController>();
-    _transform = transform;
+        _transform = transform;
+        _cameraAngle = _cameraTransform.localEulerAngles.x;
     }
 
     private void Update()
     {
+        Vector3 forward = Vector3.ProjectOnPlane(_cameraTransform.forward, Vector3.up).normalized;
+        Vector3 right = Vector3.ProjectOnPlane(_cameraTransform.right, Vector3.up).normalized;
+        
+        _cameraAngle -= Input.GetAxis("Mouse Y") * _verticalSensivity;
+        _cameraAngle = Mathf.Clamp(_cameraAngle, _verticalMinAngle, _verticalMaxAngle);
+        _cameraTransform.localEulerAngles = Vector3.right * _cameraAngle;
+        
+        _transform.Rotate(Vector3.up * __horizontalSensivity * Input.GetAxis("Mouse X"));
+        
         if (_characterController)
         {
-            Vector3 playerInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            playerInput *= Time.deltaTime * _speed;
-
+            Vector3 playerSpeed = forward * Input.GetAxis("Vertical") * _speed + right * Input.GetAxis("Horizontal") * _straveSpeed;
+            
             if (_characterController.isGrounded)
             {
-                _characterController.Move(playerInput + Vector3.down);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    _verticalVelocity = Vector3.up * _jumpSpeed;
+                }
+                else
+                {
+                    _verticalVelocity = Vector3.down;
+                }
+                
+                _characterController.Move((playerSpeed + _verticalVelocity)*Time.deltaTime);
             }
             else
             {
-                _characterController.Move(_characterController.velocity + Physics.gravity*Time.deltaTime);
+                Vector3 horizontalVelocity = _characterController.velocity;
+                horizontalVelocity.y = 0;
+                _verticalVelocity += Physics.gravity * Time.deltaTime * _gravityFactor;
+                _characterController.Move((horizontalVelocity + _verticalVelocity) * Time.deltaTime);
             }
         }
     }
--- Assets/Scripts/Player/PlayerRigidbody.cs
+++ Assets/Scripts/Player/PlayerRigidbody.cs
@@ -1,16 +1,24 @@
+using System;
 using UnityEngine;
 
 public class PlayerRigidbody : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _speed = 3f;
+   
+    
+    private Rigidbody _rigidbody;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        Vector3 playerSpeed = new Vector3(Input.GetAxis("Horizontal") * _speed, 0f, Input.GetAxis("Vertical"));
+        var character = GetComponent<CharacterController>();
+
+        _rigidbody.linearVelocity += Physics.gravity;
     }
 }
